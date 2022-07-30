ffb7aa25311d7919a021785c0641127d7bd5cbe7
MichitakaTsuda <67595708+MichitakaTsuda@users.noreply.github.com>
2022-07-30 16:29:59 +0900

    section6

---
+++ src/components/molecules/SearchInput.jsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+import { PrimaryButton } from "../atoms/buttons/PrimaryButton";
+import { Input } from "../atoms/inputs/input";
+
+export const SearchInput = () => {
+  return (
+    <div>
+      <SContainer>
+        <Input placeholder="検索条件を入力" />
+        <SButtonWrapper>
+          <PrimaryButton>Search</PrimaryButton>
+        </SButtonWrapper>
+      </SContainer>
+    </div>
+  );
+};
+
+const SButtonWrapper = styled.div`
+  padding-left: 8px;
+`;
+
+const SContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
